4fc4aa8b50aa23b954f25eda3022c12d0921f809
lisanyny <110669469+lisanyny@users.noreply.github.com>
2023-03-07 06:04:55 +0000

    jQueryの学習

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery";
+import "./top"
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
